66eea1d2aea0c29ee3b7fb89c4997501831cdfa2
Thabo0 <thabo.wmc@gmail.com>
2023-07-17 16:14:44 -0700

    C102

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,30 +1,33 @@
 #include <stdio.h>
-/** coment
- * 102 */
+/**
+ * main - int function
+ * Return: 0 (Success)
+ * Description: Print numbers
+ */
 
 int main(void)
 {
-        int i, j;
+	int i, j;
 
-        for( i = 0; j < 100; j++)
-        {
-                for(j=0; j <100; j++)
-                {
-                        if (i<j)
-                        {
-                                putchar((i/10)+48);
-				putchar((i%10)+48);
+	for (i = 0; j < 100; j++)
+	{
+		for (j = 0; j < 100; j++)
+		{
+			if (i < j)
+			{
+				putchar((i / 10) + 48);
+				putchar((i % 10) + 48);
 				putchar(' ');
-				putchar((j/10)+48);
-				putchar((j%10)+48);
-				if (i !=98||j !=99)
+				putchar((j / 10) + 48);
+				putchar((j % 10) + 48);
+				if (i != 98 || j != 99)
 				{
-                                        putchar(',');
-                                        putchar(' ');
+					putchar(',');
+					putchar(' ');
 				}
-                        }
-                }
-        }
-        putchar('\n');
-        return (0);
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
 }
