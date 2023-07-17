46df95de1d36ca4190ca8f9bbb3b0926485244a7
Thabo0 <thabo.wmc@gmail.com>
2023-07-17 15:56:18 -0700

    C101

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,31 +1,34 @@
 #include <stdio.h>
-/** coment
- * 101 */
+/**
+ * main - int function
+ * Return: 0 (Success)
+ * Description: Print numbers
+ */
 
 int main(void)
 {
-        int n, m,l;
+	int n, m, l;
 
-        for( n = 48; n <= 58; n++)
-        {
-                for( m=49; m <=58; m++)
-                {
-			for(l=50;l<58;l++)
+	for (n = 48; n <= 58; n++)
+	{
+		for (m = 49; m <= 58; m++)
+		{
+			for (l = 50; l < 58; l++)
 			{
-                        if (l>m&&m>n)
-                        {
-                                putchar(n);
-                                putchar(m);
-				putchar(l);
-                                if (n !=55||m!=56)
-                                {
-                                        putchar(',');
-                                        putchar(' ');
-                                }
-                        }
+				if (l > m && m > n)
+				{
+					putchar(n);
+					putchar(m);
+					putchar(l);
+					if (n != 55 || m != 56)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
 			}
-                }
-        }
-        putchar('\n');
-        return (0);
+		}
+	}
+	putchar('\n');
+	return (0);
 }
