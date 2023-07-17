32a54970a854fc861aa4ac342f77b1e7e137618e
Thabo0 <thabo.wmc@gmail.com>
2023-07-17 14:50:59 -0700

    g

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 /**
- * comment 6
- * */
-
+ * main - char function
+ * Return: 0 (Success)
+ * Description: Print numbers
+ */
 int main(void)
 {
 	int i;
-	for (i=48; i< 58; i++)
+
+	for (i = 48; i < 58; i++)
 	{
 		putchar(i);
 	}
